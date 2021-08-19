45ba5f85741ce75dabb346eef59a6295a18290e9
Clendon DuRapau <clendon.durapau@gmail.com>
2021-08-19 11:42:26 -0500

    Implemented GET /products/:product_id endpoint.

--- server/app.js
+++ server/app.js
@@ -12,19 +12,39 @@ app.get('/products', (req, res) => {
   const page = req.query.page ? Number(req.query.page) : 1;
   const query = {
     text: `SELECT * FROM products
-    ORDER BY id
-    LIMIT $1
-    OFFSET $2;`,
+           ORDER BY id
+           LIMIT $1
+           OFFSET $2;`,
     values: [count, (page - 1) * count],
   };
   db
     .query(query)
     .then((results) => res.status(200).send(results.rows))
-    .catch((error) => console.error(error.stack));
+    .catch((error) => {
+      console.error(error.stack);
+      res.status(400).send(error.stack);
+    });
 });
 
 app.get('/products/:product_id', (req, res) => {
-  res.send(req.params);
+  const query = {
+    text: `SELECT products.id, name, slogan, description, category, default_price,
+           array_agg(jsonb_build_object('feature', feature, 'value', value))
+             AS features
+           FROM products
+           INNER JOIN features
+           ON products.id = features.product_id
+           WHERE products.id = $1
+           GROUP BY products.id;`,
+    values: [req.params.product_id],
+  };
+  db
+    .query(query)
+    .then((results) => res.status(200).send(results.rows))
+    .catch((error) => {
+      console.error(error.stack);
+      res.status(400).send(error.stack);
+    });
 });
 
 module.exports = app;
